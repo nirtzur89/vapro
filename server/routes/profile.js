4f0e140a51198331e89537e2b11ad353512642aa
KaaKoe <k.koenig1811@gmail.com>
2019-03-09 17:23:43 +0100

    created auth.js with post method to create users

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -5,53 +5,6 @@ const router  = express.Router();
 const User = require('../models/user-model')
 
 
-//ONLY for test cases! POST should be part of the signup form instead
-router.post('/profile-artist', (req, res, next) => {
-
-    User.create({
-        type: "artist",
-        username: req.body.username,
-        firstname: req.body.name,
-        lastname: req.body.lastname,
-        email: req.body.email,
-        avatar: req.body.avatar,
-        password: req.body.password,
-        bio: req.body.bio,
-        website: req.body.website,
-        vimeo: req.body.vimeo,
-        pinterest: req.body.pinterest,
-        instagram: req.body.instagram,
-        hastags: [],
-        projects: []
-    })
-    .then(response => {
-        res.json(response);
-    })
-    .catch(err => {
-        res.json(err);
-    })
-});
-
-//ONLY for test cases! POST should be part of the signup form instead
-router.post('/profile-member', (req, res, next) => {
-    User.create({
-        type: "member",
-        username: req.body.username,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        avatar: req.body.avatar,
-        company: req.body.company,
-        password: req.body.password,
-        savedArtists: [],
-    })
-    .then(response => {
-        res.json(response);
-    })
-    .catch(err => {
-        res.json(err);
-    })
-});
 
 
 
