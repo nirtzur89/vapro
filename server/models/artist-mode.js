5309366e8bf6fca6e19a96ce3772280367cc079a
KaaKoe <k.koenig1811@gmail.com>
2019-03-09 12:23:40 +0100

    artist model

---
+++ server/models/artist-mode.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const artistModel = new Schema({
+    username: String,
+    name: String,
+    lastname: String,
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: 'Email address is required',
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    avatar: { data: Buffer, contentType: String },
+    password: String,
+    bio: String,
+    website: String,
+    vimeo: String,
+    Pinterest: String,
+    Instagram: String,
+})
+
+const Artist = mongoose.model('Artist', artistModel)
+
+module.exports = Artist;
