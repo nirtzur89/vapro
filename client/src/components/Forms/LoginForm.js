443f17c65078c377c200216ae39bb4e973007895
KaaKoe <k.koenig1811@gmail.com>
2019-03-14 21:10:09 +0100

    forms only frontend

---
+++ client/src/components/Forms/LoginForm.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './LoginForm.css';
+
+class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: '',
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+        }
+
+    }
+
+    render() {
+        return (
+            <div>
+                <form className="loginForm formDiv">
+                    <div>
+                        <input type="email" name="email" className="inputfield" placeholder="email"></input>
+                    </div>
+                    <div>
+                        <input type="password" name="password" className="inputfield" placeholder="password"></input>
+                    </div>
+                    <button type="submit" className="btn">
+                        Login
+                    </button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default LoginForm;
