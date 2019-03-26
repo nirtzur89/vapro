ff56872e0cc23e2cd2717a07ec5c68cf4097bbea
nirtzur <nirtzur89@gmail.com>
2019-03-26 14:38:06 +0100

    project list showing

---
+++ client/src/components/project/AllProjects.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+//import SearchBar from '../SearchArtists/SearchBar';
+import ProjectCard from './ProjectCard';
+
+class ProjectsList extends Component {
+  constructor(props){
+      super(props)
+      this.state = { listOfProjects: [] }
+    }
+  
+    getAllProjects = () =>{
+      axios.get(`http://localhost:5000/projects/all`)
+      .then(responseFromApi => {
+        this.setState({
+          listOfProjects: responseFromApi.data
+        })
+      })
+    }
+  
+    componentDidMount() {
+      this.getAllProjects();
+    }
+
+
+    render(){
+        return(
+          <div>
+            <div style={{width: '60%', float:"left"}}>
+              { this.state.listOfProjects.map( project => {
+                return (
+                  <div key={project._id}>
+                      <h3>{project.name}</h3>
+                    {/* <p style={{maxWidth: '400px'}} >{project.description} </p> */}
+                  </div>
+                )})
+              }
+            </div>
+            <div style={{width: '40%', float:"right"}}>
+            
+            </div>
+          </div>
+        )
+      }
+    }
+    
+
+export default ProjectsList;
