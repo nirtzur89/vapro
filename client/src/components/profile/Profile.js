953b54a5180d312f99db9802a407dcf3ff795ad9
nirtzur <nirtzur89@gmail.com>
2019-03-30 16:17:08 +0100

    add project works

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,27 +1,33 @@
-import React, { Component } from 'react';
-import AuthService from '../../components/Auth/auth-service';
-import withAuth from '../../components/Auth/withAuth';
-
+import React, { Component } from "react";
+import AuthService from "../../components/Auth/auth-service";
+import withAuth from "../../components/Auth/withAuth";
 
 const Auth = new AuthService();
 
 class Profile extends Component {
   // This method that we create in the AuthService will clear the token from localStorage.
-  handleLogout(){
-    Auth.logout()
-    this.props.history.replace('/');
- }
+  handleLogout() {
+    Auth.logout();
+    this.props.history.replace("/");
+  }
 
   render() {
-    return(
+    return (
       <div className="App">
-          <div className="App-header">
-              <h2>Welcome {this.props.user.userName}</h2>
-          </div>
-          <h1>Your Profile</h1>
-          <p>username</p>
-          <p className="App-intro">
-            <button type="button" className="form-submit" onClick={this.handleLogout.bind(this)}>Logout</button>
+        <div className="App-header">
+          <h2>Welcome {this.props.user.userName}</h2>
+          <h2>Welcome {this.props.user.password}</h2>
+        </div>
+        <h1>Your Profile</h1>
+        <p>username</p>
+        <p className="App-intro">
+          <button
+            type="button"
+            className="form-submit"
+            onClick={this.handleLogout.bind(this)}
+          >
+            Logout
+          </button>
         </p>
       </div>
     );
