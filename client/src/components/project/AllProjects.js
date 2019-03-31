d11ff22272b527f3d4542f91bd6650818d989e20
nirtzur <nirtzur89@gmail.com>
2019-03-31 19:21:59 +0200

    show my projects

--- client/src/components/project/AllProjects.js
+++ client/src/components/project/AllProjects.js
@@ -1,47 +1,47 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 //import SearchBar from '../SearchArtists/SearchBar';
 
 class ProjectsList extends Component {
-  constructor(props){
-      super(props)
-      this.state = { listOfProjects: [] }
-    }
-  
-    getAllProjects = () =>{
-      axios.get((process.env.REACT_APP_API_URL || "http://localhost:5000") + "/projects")
+  constructor(props) {
+    super(props);
+    this.state = { listOfProjects: [] };
+  }
+
+  getAllProjects = () => {
+    axios
+      .get(
+        (process.env.REACT_APP_API_URL || "http://localhost:5000") + "/projects"
+      )
       .then(responseFromApi => {
         this.setState({
           listOfProjects: responseFromApi.data
-        })
-      })
-    }
-  
-    componentDidMount() {
-      this.getAllProjects();
-    }
+        });
+      });
+  };
 
+  componentDidMount() {
+    this.getAllProjects();
+    console.log(this.state.listOfProjects);
+  }
 
-    render(){
-        return(
-          <div>
-            <div style={{width: '60%', float:"left"}}>
-              { this.state.listOfProjects.map( project => {
-                return (
-                  <div key={project._id}>
-                      <h3>{project.name}</h3>
-                    {/* <p style={{maxWidth: '400px'}} >{project.description} </p> */}
-                  </div>
-                )})
-              }
-            </div>
-            <div style={{width: '40%', float:"right"}}>
-            
-            </div>
-          </div>
-        )
-      }
-    }
-    
+  render() {
+    return (
+      <div>
+        <div style={{ width: "60%", float: "left" }}>
+          {this.state.listOfProjects.map(project => {
+            return (
+              <div key={project._id}>
+                <h3>{project.name}</h3>
+                {/* <p style={{maxWidth: '400px'}} >{project.description} </p> */}
+              </div>
+            );
+          })}
+        </div>
+        <div style={{ width: "40%", float: "right" }} />
+      </div>
+    );
+  }
+}
 
 export default ProjectsList;
