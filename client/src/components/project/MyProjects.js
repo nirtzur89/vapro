c15402786acbee9b41eb56f3a0ab407ed27853da
nirtzur <nirtzur89@gmail.com>
2019-04-02 21:52:30 +0200

    showing only my proj + delete route

--- client/src/components/project/MyProjects.js
+++ client/src/components/project/MyProjects.js
@@ -20,12 +20,37 @@ class MyProjects extends Component {
   getAllProjects = () => {
     axios
       .get(
-        (process.env.REACT_APP_API_URL || "http://localhost:5000") + "/projects"
+        (process.env.REACT_APP_API_URL || "http://localhost:5000") +
+          "/projects/myprojects",
+        {
+          headers: {
+            authorization: this.Auth.getToken()
+          }
+        }
       )
       .then(responseFromApi => {
         this.setState({
           listOfProjects: responseFromApi.data
         });
+      });
+  };
+
+  deleteProject = el => {
+    axios
+      .delete(
+        (process.env.REACT_APP_API_URL || "http://localhost:5000") +
+          `/projects/${el}`,
+        {
+          headers: {
+            authorization: this.Auth.getToken()
+          }
+        }
+      )
+      .then(() => {
+        this.props.history.push("/myprojects"); // !!!
+      })
+      .catch(err => {
+        console.log(err);
       });
   };
 
@@ -35,19 +60,22 @@ class MyProjects extends Component {
 
   render() {
     const activeUser = this.props.user.id;
+    console.log("params", this.props);
     return (
       <div>
         <h2>{activeUser}</h2>
         <div>
           {this.state.listOfProjects.map(project => {
-            if (project.artist === activeUser) {
-              return (
-                <div key={project._id}>
-                  <h3>{project.name}</h3>
-                  {/* <p style={{maxWidth: '400px'}} >{project.description} </p> */}
-                </div>
-              );
-            }
+            console.log("params in", this.state);
+            return (
+              <div key={project._id}>
+                <h3>{project.name}</h3>
+                <button onClick={() => this.deleteProject(project._id)}>
+                  Delete project
+                </button>
+                {/* <p style={{maxWidth: '400px'}} >{project.description} </p> */}
+              </div>
+            );
           })}
         </div>
         <p>Artist: </p>
